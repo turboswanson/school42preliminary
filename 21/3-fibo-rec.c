ad88922b921ba099648d9ecacdbc6cf117bb47be
Denzel Washington <turboswanson@MS-7B89-f0923ed1.localdomain>
2023-04-06 00:24:03 +0000

    modified fibo

--- 21/3-fibo-rec.c
+++ 21/3-fibo-rec.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
-int ft_scan( int a){
-    if(scanf("%d",&a) != 1){
+
+int ft_scan(int a,char c);
+int fibo_rec(int a);
+
+int main(void){
+    int a = 0;
+    char c;
+    a = ft_scan(a,c);
+    
+    if (a !=0){
+     int res = fibo_rec(a);
+     printf("%d",res);
+    }
+
+return 0;
+
+}
+
+int ft_scan(int a, char c){
+    if(scanf("%d%c",&a,&c) != 2 || c != '\n' || a == 0){
         printf("n/a\n");
         return 0;
     }
@@ -9,20 +27,9 @@ int ft_scan( int a){
 
 int fibo_rec(int a){
     if(a == 1 || a == 2){
-        return 1;
-    }
-    return fibo_rec(a-2)+fibo_rec(a-1);
-}
-
-int main(void){
-    int a = 0;
-    a = ft_scan(a);
     
-    if (a !=0){
-     int res = fibo_rec(a);
-     printf("%d",res);
+    return 1;
     }
-
-   
-
+    
+    return fibo_rec(a-2)+fibo_rec(a-1);
 }
