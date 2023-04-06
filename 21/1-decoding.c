625b23aebbd74e8adc7c255807c16a5ee2bf785b
Denzel Washington <turboswanson@MS-7B89-f0923ed1.localdomain>
2023-04-06 00:23:23 +0000

    modified decoding

--- 21/1-decoding.c
+++ 21/1-decoding.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 
-int main(void){
-    unsigned int a1;
-    char a2;
-    while(a2 != '\n'){
-        scanf("%x%c",&a1,&a2);
-        printf("%c%c",a1,a2);
-    }
-    /*char c1;
-    char c2;
-    while(c2 != '\n'){
-        scanf("%c%c",&c1,&c2);
-        printf("%x%c",c1,c2);
-    }*/
+int coding(char b,char c);
+int decoding(unsigned int z, char a);
 
+int main(int argc,char *argv[]){
+    char a = 32,b,c;
+    unsigned int z = 0;
+    
+    if (*argv[1] == '0'){
+        coding(b,c);
+    }
+
+    else{
+        decoding(z,a);
+    }
+  
     return 0;
+}
+
+int coding(char b,char c){
+    while(c != '\n'){
+            scanf("%c%c",&b,&c);
+            if(c != 32 && c != '\n'){
+                printf("n/a\n");
+                return 0;
+            }
+            printf("%x ",b);
+        }
+    }
+
+int decoding(unsigned int z, char a){    
+    while(a == 32){
+        scanf("%x%c",&z,&a);
+        
+        printf("%c ",z);
+
+        
+    }
 }
