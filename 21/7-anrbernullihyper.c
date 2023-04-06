c3a486a03914243db15a4942b173946c613f788f
Denzel Washington <turboswanson@MS-7B89-f0923ed1.localdomain>
2023-04-06 00:52:20 +0000

    modified bernulli

--- 21/7-anrbernullihyper.c
+++ 21/7-anrbernullihyper.c
@@ -2,21 +2,18 @@
 #include <math.h>
 
 double diam(double a);
-double geperb(double a);
+double hyper(double a);
 double bern(double a);
 
 int main(void) {
     double pi = 3.14159265358979323846;
     for (double i = -pi; i < pi; i += pi/21) {
-        if (diam(i) == NAN) {
-            printf("%.7lf | - | %.7lf\n", bern(i), geperb(i));
-        } else if ( bern(i) == NAN) {
-            printf("%.7lf | - | %.7lf\n", diam(i), geperb(i));
-        } else if (geperb(i) == NAN) {
-            printf("%.7lf | %.7lf | -\n", diam(i), bern(i));
-        } else {
-            printf("%.7lf | %.lf | %.7lf\n", diam(i), bern(i), geperb(i));
-        }
+        if (bern(i) != NAN) {
+            printf("%.7lf | %.7lf | %.7lf | %.7lf\n",i,diam(i), bern(i), hyper(i));
+       
+        } /*else {
+            printf("%.7lf | %.7lf | - | %.7lf\n",i, diam(i), hyper(i));
+        }*/
     }
     return 0;
 }
@@ -29,6 +26,6 @@ double bern(double a) {
     return sqrt(sqrt(1+4*a*a)-a*a-1);
 }
 
-double geperb(double a) {
+double hyper(double a) {
     return 1 / (a * a);
 }
